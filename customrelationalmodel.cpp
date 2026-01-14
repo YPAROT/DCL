9504655f45889b244cc1ed868904fa90ba097ad8
YPAROT <yann.parot@irap.omp.eu>
2026-01-14 09:12:55 +0100

    Modification de la classe CustomRelationalModel:
    -Surcharge de la méthode Data pour gérer les cellules modifiées

--- customrelationalmodel.cpp
+++ customrelationalmodel.cpp
@@ -1,4 +1,5 @@
 #include "customrelationalmodel.h"
+#include <QBrush>
 
 CustomRelationalModel::CustomRelationalModel(QObject *parent)
     : QSqlRelationalTableModel{parent}
@@ -16,4 +17,21 @@ Qt::ItemFlags CustomRelationalModel::flags(const QModelIndex &index) const
     }
 
     return defaultFlags;
+}
+
+QVariant CustomRelationalModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    //Si la valeur est changé on change le background à jaune
+    if(role == Qt::BackgroundRole)
+    {
+        if(isDirty(index))
+            return QBrush(Qt::yellow);
+
+    }
+
+    // Pour les autres rôles on appelle la méthode mère
+    return QSqlRelationalTableModel::data(index, role);
 }
--- customrelationalmodel.h
+++ customrelationalmodel.h
@@ -8,7 +8,11 @@ class CustomRelationalModel : public QSqlRelationalTableModel
     Q_OBJECT
 public:
     explicit CustomRelationalModel(QObject *parent = nullptr);
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
+
+    //override
+    virtual Qt::ItemFlags flags(const QModelIndex& index) const override;
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
 };
 
 #endif // CUSTOMRELATIONALMODEL_H
